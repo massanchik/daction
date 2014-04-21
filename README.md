739556671ece84f9a703d236ac95b5db96e445e7
TreeNode <ondaemon@gmail.com>
2014-04-22 00:19:56 +0300

    doc

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 daction
 =======
+
+Performs custom action to all/specified elements in a directory.
+
+Examples:
+-------
+
+    daction '/var/www/dir*template' --action='python some_script.py'
+    daction '/var/www/dir*template' --action='php composer.phar update'
+    daction '/var/www/dir*template' --action='vim some_file.ext'
--- daction/main.py
+++ daction/main.py
@@ -7,8 +7,8 @@ from daction.daction import perform_action
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Performs custom action on all/specified elements in the directory.')
-    parser.add_argument('path', nargs='?', help='Path to the directory. For example: "/foo/bar" or "/foo/*bar*"')
+    parser = argparse.ArgumentParser(description='Performs custom action to all/specified elements in a directory.')
+    parser.add_argument('path', nargs='?', help='Path to a directory in which you want to do some action. For example: "/foo/bar" or "/foo/*bar*"')
     parser.add_argument('--action', nargs='?', default='ls -l', help='Action to perform. For example: "ls -la"')
     parser.add_argument('--version', action='store_true', help="Print the version number and exit.")
     args = parser.parse_args()
@@ -17,7 +17,7 @@ def main():
         print(daction.version)
         return
     if not args.path:
-        print('You must supply a dir\n', file=sys.stderr)
+        print('You must supply a path\n', file=sys.stderr)
         parser.print_help()
         return
 
